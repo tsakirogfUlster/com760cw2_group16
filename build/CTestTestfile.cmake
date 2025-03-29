50f28854c48be7824172895426d93fdd2248d801
fotis <tsakirogf@hotmail.com>
2025-03-29 12:01:43 +0000

    Add catkin boilerplate code

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/fotis/Documents/ulster/com760cw2_group16/src
+# Build directory: /home/fotis/Documents/ulster/com760cw2_group16/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/fotis/Documents/ulster/com760cw2_group16/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/fotis/Documents/ulster/com760cw2_group16/devel;/opt/ros/kinetic'.split(';') if '/home/fotis/Documents/ulster/com760cw2_group16/devel;/opt/ros/kinetic' != '' else []
